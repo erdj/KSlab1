e62e3badd99b67963bc847fa0a41158e7c44c0cf
erdj <erdjanik776@gmail.com>
2023-01-28 16:41:36 +0100

    first commit

---
+++ utils.js
@@ -0,0 +1,134 @@
+export const uaAlph = [
+  {
+    lowercase: 'а',
+    uppercase: 'А',
+  },
+  {
+    lowercase: 'б',
+    uppercase: 'Б',
+  },
+  {
+    lowercase: 'в',
+    uppercase: 'В',
+  },
+  {
+    lowercase: 'г',
+    uppercase: 'Г',
+  },
+  {
+    lowercase: 'ґ',
+    uppercase: 'Ґ',
+  },
+  {
+    lowercase: 'д',
+    uppercase: 'Д',
+  },
+  {
+    lowercase: 'е',
+    uppercase: 'Е',
+  },
+  {
+    lowercase: 'є',
+    uppercase: 'Є',
+  },
+  {
+    lowercase: 'ж',
+    uppercase: 'Ж',
+  },
+  {
+    lowercase: 'з',
+    uppercase: 'З',
+  },
+  {
+    lowercase: 'и',
+    uppercase: 'И',
+  },
+  {
+    lowercase: 'і',
+    uppercase: 'І',
+  },
+  {
+    lowercase: 'ї',
+    uppercase: 'Ї',
+  },
+  {
+    lowercase: 'й',
+    uppercase: 'Й',
+  },
+  {
+    lowercase: 'к',
+    uppercase: 'К',
+  },
+  {
+    lowercase: 'л',
+    uppercase: 'Л',
+  },
+  {
+    lowercase: 'м',
+    uppercase: 'М',
+  },
+  {
+    lowercase: 'н',
+    uppercase: 'Н',
+  },
+  {
+    lowercase: 'о',
+    uppercase: 'О',
+  },
+  {
+    lowercase: 'п',
+    uppercase: 'П',
+  },
+  {
+    lowercase: 'р',
+    uppercase: 'Р',
+  },
+  {
+    lowercase: 'с',
+    uppercase: 'С',
+  },
+  {
+    lowercase: 'т',
+    uppercase: 'Т',
+  },
+  {
+    lowercase: 'у',
+    uppercase: 'У',
+  },
+  {
+    lowercase: 'ф',
+    uppercase: 'Ф',
+  },
+  {
+    lowercase: 'х',
+    uppercase: 'Х',
+  },
+  {
+    lowercase: 'ц',
+    uppercase: 'Ц',
+  },
+  {
+    lowercase: 'ч',
+    uppercase: 'Ч',
+  },
+  {
+    lowercase: 'ш',
+    uppercase: 'Ш',
+  },
+  {
+    lowercase: 'щ',
+    uppercase: 'Щ',
+  },
+  {
+    lowercase: 'ь',
+    uppercase: 'Ь',
+  },
+  {
+    lowercase: 'ю',
+    uppercase: 'Ю',
+  },
+  {
+    lowercase: 'я',
+    uppercase: 'Я',
+  },
+];
